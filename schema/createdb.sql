b581bc910b7aecbcfb79b053a06407fac647835f
Tomas <tomasholderness@gmail.com>
2016-11-02 09:30:46 +0000

    Updated schema to push grasp reports to master reports table

--- schema/createdb.sql
+++ schema/createdb.sql
@@ -5,3 +5,7 @@ CREATE DATABASE cognicity_grasp
        LC_COLLATE = 'en_US.UTF-8'
        LC_CTYPE = 'en_US.UTF-8'
        CONNECTION LIMIT = -1;
+
+-- Spatial extensions
+CREATE EXTENSION postgis;
+CREATE EXTENSION postgis_topology;
