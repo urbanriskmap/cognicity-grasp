dd3185e2ef979499349df33fbc2443669f9d6964
Tomas <tomasholderness@gmail.com>
2016-12-21 10:58:49 -0500

    Fixed missing map param

--- sample-bot-config.js
+++ sample-bot-config.js
@@ -1,7 +1,7 @@
 // Bot Config object
   var config =  {
     card_url_prefix: 'https://dev.petabencana.id/cards',
-    map_url_prefix:  'https://dev.petabencana.id',
+    map_url_prefix:  'https://dev.petabencana.id/map',
     default_language: 'en',
     network: {
       name: 'twitter'
