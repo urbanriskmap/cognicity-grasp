a01a91b2cb70c4b706de8531214064abcf5b15dd
Tomas <tomasholderness@gmail.com>
2017-01-10 21:07:16 -0500

    Updated ID translation

--- sample-bot-dialogue.js
+++ sample-bot-dialogue.js
@@ -1,17 +1,17 @@
 var dialogue = {
   ahoy: {
     en: "Hello, I am Bencana Bot, reply with #flood to send me your flood report.",
-    id: "Hello, I am Bencana Bot, reply with #banjir to send me your flood report."
+    id: "Halo, ini Bencana Bot. Kalau ada laporan banjir, ketik #laporkan."
   },
   requests: {
     card : {
-      en: 'Hello, please send me your flood report using this link',
-      id: 'Hello, please send me your flood report using this link'
+      en: 'Hi! Please send me your flood report using this link',
+      id: 'Hi! Laporan menggunakan link ini, terima kasih.'
     }
   },
   confirmation: {
-    en: "Hello, thanks for your report. I've put it on the map",
-    id: "Hello, thanks for your report. I've put it on the map"
+    en: "Hi! Thanks for your report. I've put it on the map",
+    id: "Hi! Terima kasih atas laporan Anda. Aku sudah menaruhnya di peta."
   }
 };
 
