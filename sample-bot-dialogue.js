f04caa83254de15fc366a61f6e0623bf5c47ce57
Matthew Berryman <matthew@acrossthecloud.net>
2017-01-13 02:21:08 +0000

    dialogue fixes

--- sample-bot-dialogue.js
+++ sample-bot-dialogue.js
@@ -1,7 +1,7 @@
 var dialogue = {
   ahoy: {
     en: "Hello, I am Bencana Bot, reply with #flood to send me your flood report.",
-    id: "Halo, ini Bencana Bot. Kalau ada laporan banjir, ketik #laporkan."
+    id: "Halo, saya BencanaBot. Untuk melaporkan banjir di sekitarmu, silakan balas dengan #banjir."
   },
   requests: {
     card : {
@@ -11,7 +11,7 @@ var dialogue = {
   },
   confirmation: {
     en: "Hi! Thanks for your report. I've put it on the map",
-    id: "Hi! Terima kasih atas laporan Anda. Aku sudah menaruhnya di peta."
+    id: "Hi! Terima kasih atas laporan Anda. Anda dapat melihatnya di peta berikut."
   }
 };
 
